0437b1759a83165e3aba60cf5ffed8f46bad88d7
oiknow <blacksp@126.com>
2022-06-29 17:17:26 +0800

    second commit

--- daily/src/Test01.java
+++ daily/src/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println();
+        System.out.println("第二次提交");
     }
 }
